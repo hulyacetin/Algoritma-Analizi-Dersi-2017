f4feab9489fc9ef185ce6364410968dc7180037a
Hülya Çetin <hulyacetin12@gmail.com>
2017-03-03 13:07:27 +0200

    Create pow_linear.py

---
+++ pow_linear.py
@@ -0,0 +1,19 @@
+def pow_log_n(x,n):
+    """karmaşıklık = log n"""
+    
+    if(n==0):
+        return 1
+    elif(n==1):
+        return x
+    elif(n%2==0):
+        
+        return pow(x*x,int(n/2))
+    else:
+       
+        return pow(x*x,int(n/2))*x
+
+def pow_linear(x,n):
+    result=1
+    for i in range(n):
+        result=result*x
+    return result
