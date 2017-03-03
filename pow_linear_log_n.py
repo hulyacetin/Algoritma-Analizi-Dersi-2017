7d3c4a58c74eccc8749c67a57befd6845cfef2a1
Hülya Çetin <hulyacetin12@gmail.com>
2017-03-03 13:10:00 +0200

    Update pow_linear_log_n.py

--- pow_linear_log_n.py
+++ pow_linear_log_n.py
@@ -1,15 +1,18 @@
 def pow_log_n(x,n):
     """karmaşıklık = log n"""
+    global s
     
     if(n==0):
+        s=s+1
         return 1
     elif(n==1):
+        s=s+1
         return x
     elif(n%2==0):
-        
+        s=s+1
         return pow(x*x,int(n/2))
     else:
-       
+        s=s+1
         return pow(x*x,int(n/2))*x
 
 def pow_linear_log_n(x,n):
